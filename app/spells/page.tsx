31b71215ac6464acad3ecc48d99ca383e8606e5e
Ilya Nikitin <ilya.ninja@gmail.com>
2024-03-19 17:06:51 +0200

    search field onChange fix
    search field onChange fix

--- app/spells/page.tsx
+++ app/spells/page.tsx
@@ -4,10 +4,17 @@ import map from 'lodash/map';
 
 import { SpellsList } from '@/components/SpellsList';
 import { spellLevels } from '@/components/SpellsList/SpellsConstants';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export default function Spells() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [spellName, setSpellName] = useState('');
+
+  const handleOnChange = useCallback(
+    (event) => {
+      setSpellName(event.target.value);
+    },
+    [spellName, setSpellName]
+  );
 
   return (
     <div>
@@ -17,7 +24,7 @@ export default function Spells() {
           <input
             className="bg-zinc-100 rounded-md px-2 border border-zinc-200 placeholder:text-zinc-400 py-2 focus:outline-none"
             placeholder="Spell name"
-            onChange={(event) => setSearchTerm(event.target.value)}
+            onChange={handleOnChange}
           />
         </div>
       </div>
@@ -27,7 +34,7 @@ export default function Spells() {
             key={spellLevel.level}
             spellLevel={spellLevel.level}
             color={spellLevel.color}
-            name={searchTerm}
+            name={spellName}
           />
         ))}
       </div>
